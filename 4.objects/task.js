dd03b2e8e6fbe5c81df4a6ab0a50c4a60fe0cda2
Roman Sipeta <rsipeta@yandex.ru>
2022-05-22 22:10:05 +0300

    Выполнена четвертая работа

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,34 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;  
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [ mark ]; 
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  marks.forEach(element => {this.addMark(element);});
+}
+
+Student.prototype.getAverage = function(){
+  if (this.marks.length == 0) return 0;
+  let sum = 0;
+  this.marks.forEach(element => { sum += element;  });
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason){
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
